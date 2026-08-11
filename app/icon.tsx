1736dc09b92338e047b1468aeaf9743ceb73a530
gcus-nm <gcusnm12@gmail.com>
2026-08-12 00:09:18 +0900

    fix: faviconをページ内アイコンに統一

--- app/icon.tsx
+++ app/icon.tsx
@@ -17,38 +17,23 @@ export default function Icon() {
           alignItems: "center",
           justifyContent: "center",
           background: "#8f2f22",
-          borderRadius: 96,
+          borderRadius: 154,
           color: "#fffaf1",
         }}
       >
         <svg
-          width="340"
-          height="340"
-          viewBox="0 0 340 340"
+          width="278"
+          height="278"
+          viewBox="0 0 24 24"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
+          stroke="#fff9f1"
+          strokeWidth="2.4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         >
-          <path
-            d="M170 105C139 75 98 63 51 66V250C101 247 141 260 170 287V105Z"
-            fill="#fffaf1"
-          />
-          <path
-            d="M170 105C201 75 242 63 289 66V250C239 247 199 260 170 287V105Z"
-            fill="#fffaf1"
-          />
-          <path
-            d="M51 250C101 247 141 260 170 287C199 260 239 247 289 250"
-            stroke="#d6a755"
-            strokeWidth="18"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-          <path
-            d="M170 105V287"
-            stroke="#8f2f22"
-            strokeWidth="14"
-            strokeLinecap="round"
-          />
+          <path d="M12 7v14" />
+          <path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z" />
         </svg>
       </div>
     ),
